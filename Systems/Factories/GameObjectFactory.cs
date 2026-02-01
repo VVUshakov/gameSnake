5e5810c6a579e2e102ac6f3f60ed4f2ed806e376
Владимир Ушаков <svayznoy@inbox.ru>
2026-02-01 23:51:41 +0700

    Фабрика для создания игровых объектов

--- Systems/Factories/GameObjectFactory.cs
+++ Systems/Factories/GameObjectFactory.cs
@@ -1,9 +1,8 @@
-﻿using gameSnake.Core;
-using gameSnake.GameObjects.Food;
-using gameSnake.GameObjects.Snake;
-using gameSnake.Interfaces;
+﻿using SnakeGame.Core;
+using SnakeGame.GameObjects;
+using SnakeGame.Interfaces;
 
-namespace gameSnake.Systems.Factories
+namespace SnakeGame.Systems.Factories
 {
     /// <summary>
     /// Фабрика для создания игровых объектов
