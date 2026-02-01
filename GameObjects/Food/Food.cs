46fd128de01e2513d08eaf4090955ed04b29f79e
Владимир Ушаков <svayznoy@inbox.ru>
2026-02-01 23:14:15 +0700

    # Класс, представляющий еду на игровом поле

---
+++ GameObjects/Food/Food.cs
@@ -0,0 +1,49 @@
+﻿using gameSnake.Core;
+using gameSnake.Interfaces;
+
+namespace gameSnake.GameObjects.Food
+{
+    /// <summary>
+    /// Класс, представляющий еду на игровом поле
+    /// </summary>
+    public class Food : IGameObject
+    {
+        private static readonly Random _random = new Random();
+
+        /// <summary>
+        /// Текущая позиция еды на поле
+        /// </summary>
+        public Position Position { get; private set; }
+
+        /// <summary>
+        /// Создает новую еду в случайной позиции
+        /// </summary>
+        /// <param name="width">Ширина игрового поля</param>
+        /// <param name="height">Высота игрового поля</param>
+        public Food(int width, int height)
+        {
+            Respawn(width, height);
+        }
+
+        /// <summary>
+        /// Перемещает еду в новую случайную позицию
+        /// </summary>
+        /// <param name="width">Ширина игрового поля</param>
+        /// <param name="height">Высота игрового поля</param>
+        public void Respawn(int width, int height)
+        {
+            Position = new Position(
+                _random.Next(0, width),
+                _random.Next(0, height)
+            );
+        }
+
+        /// <summary>
+        /// Обновляет состояние еды (реализация IGameObject)
+        /// </summary>
+        public void Update()
+        {
+            // Еда не требует обновления, так как статична
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,49 +6,7 @@ namespace SnakeGame
 
 
 
-    /// <summary>
-    /// Класс, представляющий еду на игровом поле
-    /// </summary>
-    public class Food : IGameObject
-    {
-        private static readonly Random _random = new Random();
 
-        /// <summary>
-        /// Текущая позиция еды на поле
-        /// </summary>
-        public Position Position { get; private set; }
-
-        /// <summary>
-        /// Создает новую еду в случайной позиции
-        /// </summary>
-        /// <param name="width">Ширина игрового поля</param>
-        /// <param name="height">Высота игрового поля</param>
-        public Food(int width, int height)
-        {
-            Respawn(width, height);
-        }
-
-        /// <summary>
-        /// Перемещает еду в новую случайную позицию
-        /// </summary>
-        /// <param name="width">Ширина игрового поля</param>
-        /// <param name="height">Высота игрового поля</param>
-        public void Respawn(int width, int height)
-        {
-            Position = new Position(
-                _random.Next(0, width),
-                _random.Next(0, height)
-            );
-        }
-
-        /// <summary>
-        /// Обновляет состояние еды (реализация IGameObject)
-        /// </summary>
-        public void Update()
-        {
-            // Еда не требует обновления, так как статична
-        }
-    }
 
     // ==================== СИСТЕМЫ И СЕРВИСЫ ====================
 
