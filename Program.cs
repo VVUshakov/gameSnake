3fa44e584a449b099acf067bb7a8c90aa3966d77
Владимир Ушаков <svayznoy@inbox.ru>
2026-02-01 23:20:49 +0700

    # Фабрика для создания игровых объектов

--- Program.cs
+++ Program.cs
@@ -12,33 +12,7 @@ namespace SnakeGame
 
 
 
-    /// <summary>
-    /// Фабрика для создания игровых объектов
-    /// </summary>
-    public class GameObjectFactory : IGameObjectFactory
-    {
-        /// <summary>
-        /// Создает новый объект еды
-        /// </summary>
-        /// <param name="width">Ширина игрового поля</param>
-        /// <param name="height">Высота игрового поля</param>
-        /// <returns>Новый объект еды</returns>
-        public Food CreateFood(int width, int height)
-        {
-            return new Food(width, height);
-        }
 
-        /// <summary>
-        /// Создает новый объект змейки
-        /// </summary>
-        /// <param name="initialLength">Начальная длина змейки</param>
-        /// <returns>Новый объект змейки</returns>
-        public Snake CreateSnake(int initialLength)
-        {
-            var settings = new GameSettings();
-            return new Snake(settings.InitialSnakePosition, initialLength);
-        }
-    }
 
 
 
---
+++ Systems/Factories/GameObjectFactory.cs
@@ -0,0 +1,35 @@
+﻿using gameSnake.Core;
+using gameSnake.GameObjects.Food;
+using gameSnake.GameObjects.Snake;
+using gameSnake.Interfaces;
+
+namespace gameSnake.Systems.Factories
+{
+    /// <summary>
+    /// Фабрика для создания игровых объектов
+    /// </summary>
+    public class GameObjectFactory : IGameObjectFactory
+    {
+        /// <summary>
+        /// Создает новый объект еды
+        /// </summary>
+        /// <param name="width">Ширина игрового поля</param>
+        /// <param name="height">Высота игрового поля</param>
+        /// <returns>Новый объект еды</returns>
+        public Food CreateFood(int width, int height)
+        {
+            return new Food(width, height);
+        }
+
+        /// <summary>
+        /// Создает новый объект змейки
+        /// </summary>
+        /// <param name="initialLength">Начальная длина змейки</param>
+        /// <returns>Новый объект змейки</returns>
+        public Snake CreateSnake(int initialLength)
+        {
+            var settings = new GameSettings();
+            return new Snake(settings.InitialSnakePosition, initialLength);
+        }
+    }
+}
