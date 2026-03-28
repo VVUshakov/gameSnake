dde119308f402f724ece8e6403a089baba203b4a
Ushakov Vladimir <ushakov_vv@list.ru>
2026-03-29 00:19:24 +0700

    Добавить XML-документацию к PlayingField

--- PlayingField.cs
+++ PlayingField.cs
@@ -1,10 +1,26 @@
 ﻿namespace Snake
 {
+    /// <summary>
+    /// Представляет игровое поле.
+    /// Определяет размеры поля (ширину и высоту).
+    /// </summary>
     public class PlayingField
     {
+        /// <summary>
+        /// Ширина игрового поля в клетках
+        /// </summary>
         public int Width { get; }
+
+        /// <summary>
+        /// Высота игрового поля в клетках
+        /// </summary>
         public int Height { get; }
 
+        /// <summary>
+        /// Инициализирует новое игровое поле с указанными размерами
+        /// </summary>
+        /// <param name="width">Ширина поля в клетках (по умолчанию 20)</param>
+        /// <param name="height">Высота поля в клетках (по умолчанию 10)</param>
         public PlayingField(int width = 20, int height = 10)
         {
             Width = width;
