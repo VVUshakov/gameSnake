66582b67277b267aed7927df9762d9983864c6ce
Ushakov Vladimir <ushakov_vv@list.ru>
2026-04-03 23:54:42 +0700

    Внедрить Random в FoodSpawner через параметр

--- Utils/FoodSpawner.cs
+++ Utils/FoodSpawner.cs
@@ -7,28 +7,30 @@ namespace gameSnake.Utils
     /// </summary>
     public static class FoodSpawner
     {
-        private static readonly Random _random = new Random();
-
         /// <summary>
         /// Создаёт еду в случайном свободном месте на поле.
         /// </summary>
-        public static Food CreateFood(PlayingField field, Snake snake)
+        /// <param name="field">Игровое поле</param>
+        /// <param name="snake">Змейка</param>
+        /// <param name="random">Генератор случайных чисел (опционально)</param>
+        public static Food CreateFood(PlayingField field, Snake snake, Random? random = null)
         {
-            Point? position = FindFreePosition(field, snake);
+            random ??= new Random();
+            Point? position = FindFreePosition(field, snake, random);
             return new Food(position, position != null);
         }
 
         /// <summary>
         /// Ищет случайную свободную позицию между рамками поля.
         /// </summary>
-        private static Point? FindFreePosition(PlayingField field, Snake snake)
+        private static Point? FindFreePosition(PlayingField field, Snake snake, Random random)
         {
             int maxAttempts = 1000;
 
             for(int attempt = 0; attempt < maxAttempts; attempt++)
             {
-                int x = _random.Next(field.Left + 1, field.Right);
-                int y = _random.Next(field.Top + 1, field.Bottom);
+                int x = random.Next(field.Left + 1, field.Right);
+                int y = random.Next(field.Top + 1, field.Bottom);
                 Point candidate = new Point(x, y);
 
                 if(!snake.Contains(candidate))
