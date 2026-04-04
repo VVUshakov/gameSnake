4765a7b6f6e0ec9e28cd0e33b94791bb6f4cba4d
Ushakov Vladimir <ushakov_vv@list.ru>
2026-04-04 17:30:06 +0700

    ServiseMessange: вынести методы сообщений за атрибут MessageInfo

---
+++ Servises/MessageInfoAttribute.cs
@@ -0,0 +1,8 @@
+namespace gameSnake.Servises
+{
+    /// <summary>
+    /// Атрибут-маркер для обозначения методов, возвращающих сообщения
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public class MessageInfoAttribute : Attribute { }
+}
