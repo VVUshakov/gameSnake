8033b5bdf532cb11f998a4b08ed77d1837839cfd
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-04-08 23:49:59 +0700

    Delete Attributes directory

--- Attributes/HeaderInfoAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace gameSnake.Attributes
-{
-    /// <summary>
-    /// Атрибут-маркер для обозначения свойств, отображаемых в заголовке
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
-    public class HeaderInfoAttribute : Attribute { }
-}
--- Attributes/MessageInfoAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace gameSnake.Attributes
-{
-    /// <summary>
-    /// Атрибут-маркер для обозначения методов, возвращающих сообщения
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
-    public class MessageInfoAttribute : Attribute { }
-}
