ff92b618a5a9813784832a4f066d0a2b09343d9e
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-04-08 23:50:44 +0700

    Delete Models directory

--- Models/Point.cs
+++
@@ -1,8 +0,0 @@
-namespace gameSnake.Models
-{
-    /// <summary>
-    /// Представляет точку на игровом поле с координатами X и Y.
-    /// Является типом-значением с семантикой сравнения по значению.
-    /// </summary>
-    public record struct Point(int X, int Y);
-}
